48cedab09eb095e41f72f5825120b3436694d2b4
jmunyan <50812149+jmunyan@users.noreply.github.com>
2026-01-20 21:29:50 -0700

    Modify pi_setup.sh for Chromium startup script
    Update pi_setup.sh to create a startup script for Chromium in kiosk mode.

--- AUTOMATIONS/pi_setup.sh
+++ AUTOMATIONS/pi_setup.sh
@@ -1,3 +1,6 @@
- cd Desktop
- exho nano pi_startup.sh
- 
+#don't try this, just getting a head start. It's close but I need to double check file locations and types.
+
+cd Desktop
+echo "chromium --start-fullscreen --app=\"https://drive.google.com/drive/folders/1fwmyBGCLWtqj9Qj5nIgJ5-Z1_jsO3w01?usp=sharing\" --no-sandbox --disable-extensions --kiosk" > ppc_pi_startup.sh
+chmod +x ppc_pi_startup.sh
+echo "~/Desktop/ppc_pi_starup.sh" >> ~/.config/labwc/autostart
